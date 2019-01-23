d289679d50d218b9dfa88484d305c8ca16050b39
Matthew Jones <mmj744@cs.bham.ac.uk>
2019-01-23 09:51:30 +0000

    Reformatted to fit google java style

--- src/main/java/audio/Sounds.java
+++ src/main/java/audio/Sounds.java
@@ -1,16 +1,17 @@
 package audio;
 
 public enum Sounds {
-	// The filepaths of the sounds
-	intro("src/main/resources/audio/pacman_beginning.wav"),
-	chomp("src/main/resources/audio/pacman_chomp.wav"),
-	death("src/main/resources/audio/pacman_death.wav");
-	private final String path;
-	Sounds(String path) {
-		this.path=path;
-	}
-	
-	public String getPath() {
-		return path;
-	}
+  // The filepaths of the sounds
+  intro("src/main/resources/audio/pacman_beginning.wav"),
+  chomp("src/main/resources/audio/pacman_chomp.wav"),
+  death("src/main/resources/audio/pacman_death.wav");
+  private final String path;
+
+  Sounds(String path) {
+    this.path = path;
+  }
+
+  public String getPath() {
+    return path;
+  }
 }
