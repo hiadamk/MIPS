ec4221286ab95b4f86c180441c1a32bfce503630
MMJ744 <MMJ744@cs.bham.ac.uk>
2019-01-30 14:19:51 +0000

    implemented main menu into client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,11 +1,14 @@
 package client;
 
+import audio.AudioController;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import server.Telemetry;
+import ui.MenuController;
 import utils.Input;
 import utils.enums.Direction;
 
@@ -14,6 +17,8 @@ public class Client extends Application {
   private int id;
   private KeyController keyController;
   private Telemetry telemetry;
+  private AudioController audioController;
+
   public int getId() {
     return id;
   }
@@ -21,12 +26,16 @@ public class Client extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     int id = 0; // This will be changed if client joins a lobby, telemetry will give it new id
+    audioController = new AudioController();
     keyController = new KeyController();
-    Group root = new Group();
-    Scene scene = new Scene(root, 500, 500);
+    Scene dummyScene = new Scene(new Label("place holder"), 1920, 1080);
+    MenuController menuController = new MenuController(audioController, primaryStage, dummyScene);
+    StackPane root = (StackPane) menuController.createMainMenu();
+    Scene scene = new Scene(root, 1920, 1080);
+    scene.setOnKeyPressed(keyController);
     primaryStage.setScene(scene);
     primaryStage.show();
-    scene.setOnKeyPressed(keyController);
+
     // Main menu code will be here
 
     // If hosting if not telemetry will be set by connection method along with new client id
