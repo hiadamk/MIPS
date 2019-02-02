98d825aa10860dccd44cf68982f6930114e40e8b
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2019-02-02 21:02:32 +0000

    Refactored MapColour into new Enum MapElement which handles map representation in PNG and Array formats.

--- src/main/java/utils/Renderable.java
+++ src/main/java/utils/Renderable.java
@@ -1,16 +1,15 @@
 package utils;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import javafx.scene.image.Image;
 import utils.enums.Direction;
 
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
-
 public interface Renderable {
-    
-    public Point2D.Double getLocation();
-    
-    public ArrayList<Image> getImage();
-    
-    public Direction getDirection();
+
+  Point2D.Double getLocation();
+
+  ArrayList<Image> getImage();
+
+  Direction getDirection();
 }
---
+++ src/main/java/utils/enums/MapElement.java
@@ -0,0 +1,49 @@
+package utils.enums;
+
+public enum MapElement {
+
+  FLOOR(0, -1) {
+    @Override
+    public String toString() {
+      return "floor";
+    }
+  },
+
+  WALL(1, -16777216) {
+    public String toString() {
+      return "wall";
+    }
+  },
+
+  SPAWNPOINT(1, -16777216) {
+    public String toString() {
+      return "spawnpoint";
+    }
+  };
+
+  private int id;
+
+  private int colour;
+
+  MapElement(int id, int colour) {
+    this.id = id;
+    this.colour = colour;
+  }
+
+  public static int colourToID(int colour) {
+    for (MapElement m : MapElement.values()) {
+      if (colour == m.toColour()) {
+        return m.id;
+      }
+    }
+    return 0;
+  }
+
+  public int toInt() {
+    return id;
+  }
+
+  public int toColour() {
+    return colour;
+  }
+}
