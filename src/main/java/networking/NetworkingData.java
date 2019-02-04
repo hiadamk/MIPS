0f21e2a44ba59214ff548cd03bbc1723b3a5d502
James Weir <jjw718@student.bham.ac.uk>
2019-02-04 21:08:48 +0000

    wrapped network functionality in lobby and lobbyjoiner classes.

--- src/main/java/networking/NetworkingData.java
+++ src/main/java/networking/NetworkingData.java
@@ -4,13 +4,16 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public final class NetworkingData {
-	public static final String PREFIX = "SMSG";
-	public static final String SUFFIX = "EMSG";
-	public static final String POSITION_CODE = "POS";
-	public static final String COLLISION_CODE = "COS";
-	public static final String STOP_CODE = "EXIT"; 
-	public static final int STRING_LIMIT = 24;
-	public static final Charset CHARSET = StandardCharsets.US_ASCII;
-	public static final int PORT = 3232; // the client port that server sends to.
-	public static final String START_CODE = "START";
+
+  public static final String PREFIX = "SMSG";
+  public static final String SUFFIX = "EMSG";
+  public static final String POSITION_CODE = "POS";
+  public static final String COLLISION_CODE = "COS";
+  public static final String STOP_CODE = "EXIT";
+  public static final int STRING_LIMIT = 24;
+  public static final Charset CHARSET = StandardCharsets.US_ASCII;
+  public static final int LOBBY_PORT = 2468;
+  public static final int PORT = 3232; // the client port that server sends to.
+  public static final String START_CODE = "START";
+  //public static final String JOIN_CODE = "JOIN";
 }
