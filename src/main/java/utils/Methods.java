ef046acab9e3a36cd04b650067ea946d3c13d38e
MMJ744 <MMJ744@cs.bham.ac.uk>
2019-02-05 22:40:14 +0000

    Made a method for validating direction (Extrapolated the physics part) and added more main game loop code

--- src/main/java/utils/Methods.java
+++ src/main/java/utils/Methods.java
@@ -1,6 +1,8 @@
 package utils;
 
+import java.awt.geom.Point2D;
 import objects.Entity;
+import utils.enums.Direction;
 
 public class Methods {
 
@@ -8,5 +10,25 @@ public class Methods {
     for (Entity e : entities) {
       e.updateImages(r);
     }
+  }
+
+  public static boolean validiateDirection(Direction d, Entity e, Map m) {
+    Point2D.Double newLoc = new Point2D.Double(e.getLocation().getX(),
+        e.getLocation().getY());
+    switch (e.getDirection()) {
+      case RIGHT:
+        newLoc.setLocation(newLoc.getX() + e.getVelocity(), newLoc.getY());
+        break;
+      case LEFT:
+        newLoc.setLocation(newLoc.getX() - e.getVelocity(), newLoc.getY());
+        break;
+      case DOWN:
+        newLoc.setLocation(newLoc.getX(), newLoc.getY() + e.getVelocity());
+        break;
+      case UP:
+        newLoc.setLocation(newLoc.getX(), newLoc.getY() - e.getVelocity());
+        break;
+    }
+    return m.isWall(newLoc);
   }
 }
