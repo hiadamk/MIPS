a1d54b3878329e8fd27b90604424548ab3277425
tcc746 <tcc746@student.bham.ac.uk>
2019-02-11 02:31:22 +0000

    added resolution scaling for rendering testing code

--- src/test/java/utils/ImageTester.java
+++ src/test/java/utils/ImageTester.java
@@ -11,6 +11,7 @@ import objects.Entity;
 import renderer.Renderer;
 import utils.enums.Direction;
 
+import java.awt.*;
 import java.awt.geom.Point2D.Double;
 
 public class ImageTester extends Application {
@@ -21,30 +22,30 @@ public class ImageTester extends Application {
 
   @Override
   public void start(Stage stage) {
-    final Canvas canvas = new Canvas(1920, 1080);
+    Dimension screenRes = Toolkit.getDefaultToolkit().getScreenSize();
+    int xRes = screenRes.width;
+    int yRes = screenRes.height;
+    final Canvas canvas = new Canvas(xRes, yRes);
     final GraphicsContext gc = canvas.getGraphicsContext2D();
 
     ResourceLoader resourceLoader = new ResourceLoader("src/test/resources/");
     
       resourceLoader.loadMap("9x9");
     Map map = resourceLoader.getMap();
-      Renderer r = new Renderer(gc, 1920, 1080, resourceLoader);
+      Renderer r = new Renderer(gc, xRes, yRes, resourceLoader);
 
 
 
     Entity mip = new Entity(true, 1, new Double(1, 2));
     mip.setPacMan(true);
 
-    Entity ghoul = new Entity(false, 4, new Double(1, 5));
+    Entity ghoul = new Entity(false, 4, new Double(3, 4));
     System.out.println(ghoul.getLocation().toString());
     ghoul.setPacMan(false);
-    ghoul.setDirection(Direction.RIGHT);
+    //ghoul.setDirection(Direction.RIGHT);
 
     Entity[] entities = new Entity[]{ghoul};
     Methods.updateImages(entities, resourceLoader);
-
-    Image background = resourceLoader.getBackground();
-    gc.drawImage(background, 0, 0, 1920, 1080);
 
     r.render(map, entities);
     stage.setScene(new Scene(new Group(canvas)));
