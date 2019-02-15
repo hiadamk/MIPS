55639a3ab4e980ed3cac2cde208ffc84ca51f0ad
Alex Banks <ajb769@cs.bham.ac.uk>
2019-02-15 13:19:29 +0000

    Point logic error patches

--- src/main/java/utils/Point.java
+++ src/main/java/utils/Point.java
@@ -24,6 +24,16 @@ public class Point {
     this.MAX_X = map.getMaxX();
     this.MAX_Y = map.getMaxY();
     this.mapped = true;
+    mod();
+  }
+
+  public Point(double x, double y, int max_x, int max_y) {
+    this.x = x;
+    this.y = y;
+    this.MAX_X = max_x;
+    this.MAX_Y = max_y;
+    this.mapped = true;
+    mod();
   }
 
   public double getX() {
@@ -37,32 +47,46 @@ public class Point {
   public void setLocation(double x, double y) {
     this.x = x;
     this.y = y;
+    mod();
   }
 
   public void increaseX(double offset) {
-    x = (x + offset + MAX_X) % MAX_X;
+    x += offset;
     y = (int) y + 0.5;
+    mod();
   }
 
   public void increaseY(double offset) {
-    y = (y + offset + MAX_Y) % MAX_Y;
+    y += offset;
     x = (int) x + 0.5;
+    mod();
   }
 
   public boolean inRange(Point p) {
-    Point temp = p;
-    temp.increaseX(-this.x);
-    temp.increaseY(-this.y);
+    Point temp = new Point(this.x - p.getX(), this.y - p.getY(), this.MAX_X, this.MAX_Y);
     return (abs(temp.getX()) <= 0.5 && abs(temp.getY()) <= 0.5);
   }
 
   public String toString() {
     return "[Point] x = " + x + ", y = " + y;
   }
-  
+
   public double distance(Point to) {
     double aSquared = Math.pow(this.x - to.getX(), 2);
     double bSquared = Math.pow(this.y - to.getY(), 2);
     return Math.sqrt(aSquared + bSquared);
+  }
+
+  private void mod() {
+    if (mapped) {
+      while (this.x < 0) {
+        x += MAX_X;
+      }
+      while (this.y < 0) {
+        y += MAX_Y;
+      }
+      x = x % MAX_X;
+      y = y % MAX_Y;
+    }
   }
 }
