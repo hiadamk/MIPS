593a59cec4639f99e8668c3b5ee832e6318390f4
MMJ744 <MMJ744@cs.bham.ac.uk>
2019-02-16 19:33:18 +0000

    rewrote method using new Point class features and fixed jumping at junctions

--- src/main/java/utils/Methods.java
+++ src/main/java/utils/Methods.java
@@ -12,24 +12,12 @@ public class Methods {
   }
 
   public static boolean validiateDirection(Direction d, Entity e, Map m) {
-    Point newLoc = new Point(e.getLocation().getX(), e.getLocation().getY());
-    //    double xpart = mod(newLoc.getX(), 1);
-    //    double ypart = mod(newLoc.getY(), 1);
-    // if( ypart >= 0.60 || ypart <= 0.40 || xpart >= 0.60 || xpart <= 0.40) return false;
-    double offset = 0.8 + e.getVelocity();
-    switch (d) {
-      case RIGHT:
-        newLoc.setLocation(newLoc.getX() + offset, newLoc.getY());
-        break;
-      case LEFT:
-        newLoc.setLocation(newLoc.getX() - offset, newLoc.getY());
-        break;
-      case DOWN:
-        newLoc.setLocation(newLoc.getX(), newLoc.getY() + offset);
-        break;
-      case UP:
-        newLoc.setLocation(newLoc.getX(), newLoc.getY() - offset);
-        break;
+    Point newLoc = e.moveInDirection(0.6, d);
+    Point faceLoc = e.getFaceLocation();
+    double xpart = faceLoc.getX() % 1;
+    double ypart = faceLoc.getY() % 1;
+    if (ypart >= 0.60 || ypart <= 0.40 || xpart >= 0.60 || xpart <= 0.40) {
+      return false;
     }
     return !m.isWall(newLoc);
   }
