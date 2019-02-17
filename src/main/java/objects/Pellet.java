24335fc5d2b7d9e1e70e8ee1237d2504df4fcc37
MMJ744 <MMJ744@cs.bham.ac.uk>
2019-02-17 22:17:29 +0000

    added pellet object

---
+++ src/main/java/objects/Pellet.java
@@ -0,0 +1,37 @@
+package objects;
+
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+import utils.Point;
+import utils.Renderable;
+import utils.enums.Direction;
+
+public class Pellet implements Renderable {
+
+  private Point location;
+  private ArrayList<Image> currentImage;
+  private boolean active;
+
+  public Pellet(double x, double y) {
+    this.location = new Point(x, y);
+    active = true;
+  }
+
+  public Pellet(Point p) {
+    this.location = p;
+    active = true;
+  }
+
+  public Point getLocation() {
+    return location;
+  }
+
+  @Override
+  public ArrayList<Image> getImage() {
+    return currentImage;
+  }
+
+  public Direction getDirection() {
+    return null;
+  }
+}
