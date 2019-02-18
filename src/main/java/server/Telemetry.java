66586a1f3e725941c44cd3a3308cd904504f8e73
Matthew Jones <mmj744@cs.bham.ac.uk>
2019-02-18 14:14:33 +0000

    removed the client queue, architecture has changed since then

--- src/main/java/server/Telemetry.java
+++ src/main/java/server/Telemetry.java
@@ -3,7 +3,6 @@ package server;
 import ai.AILoopControl;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -31,7 +30,6 @@ public class Telemetry {
   }
 
   private HashMap<String, Pellet> pellets;
-  private Queue<Input> clientQueue;
   private ServerGameplayHandler server;
   private AILoopControl ai;
   private boolean aiRunning;
@@ -49,12 +47,11 @@ public class Telemetry {
     startGame();
   }
 
-  public Telemetry(Map map, Queue<Input> clientQueue, ResourceLoader resourceLoader) {
+  public Telemetry(Map map, ResourceLoader resourceLoader) {
     this.map = map;
     inputs = new LinkedBlockingQueue<>();
     outputs = new LinkedBlockingQueue<>();
     singlePlayer = true;
-    this.clientQueue = clientQueue;
     this.resourceLoader = resourceLoader;
     initialise();
     startGame();
@@ -234,14 +231,7 @@ public class Telemetry {
   }
 
   private void informClients() {
-    while (!outputs.isEmpty()) {
-      Input input = outputs.poll();
-      if (singlePlayer) {
-        clientQueue.add(input);
-      } else {
-        server.sendPacket(input);
-      }
-    }
+
   }
 
   private void updateClients() {
