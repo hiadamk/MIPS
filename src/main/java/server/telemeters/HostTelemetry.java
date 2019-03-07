4214fd5f6c9638f21051dbced8b84114eb26c547
lja781 <lja781@cs.bham.ac.uk>
2019-03-07 17:28:47 +0000

    adjusted order of AI initialisation

--- src/main/java/server/telemeters/HostTelemetry.java
+++ src/main/java/server/telemeters/HostTelemetry.java
@@ -65,6 +65,8 @@ public class HostTelemetry extends Telemetry {
       agents[(new Random()).nextInt(AGENT_COUNT)].setMipsman(true);
     }
 
+    initialisePellets();
+
     int aiCount = AGENT_COUNT - playerCount;
     if (aiCount > 0) {
       int[] aiControlled = new int[aiCount];
@@ -74,10 +76,8 @@ public class HostTelemetry extends Telemetry {
         highestId--;
       }
       aiRunning = false;
-      ai = new AILoopControl(agents, aiControlled, map, inputs);
+      ai = new AILoopControl(agents, aiControlled, map, inputs, pellets);
     }
-
-    initialisePellets();
   }
 
   public void addInput(Input in) {
