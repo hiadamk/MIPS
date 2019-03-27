71501d15100a4dbbe524ad89d8f405d1b82d4726
MMJ744 <MMJ744@cs.bham.ac.uk>
2019-03-27 15:16:01 +0000

    Made it 10x simpler and better and removed redundant stuff :(

--- src/main/java/com/lordsofmidnight/audio/AudioController.java
+++ src/main/java/com/lordsofmidnight/audio/AudioController.java
@@ -1,14 +1,11 @@
 package com.lordsofmidnight.audio;
 
 import com.lordsofmidnight.utils.Settings;
-import java.io.InputStream;
-import java.util.ArrayList;
+import java.net.URISyntaxException;
 import javafx.scene.media.AudioClip;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.FloatControl;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 /**
  * A simple system for playing sounds in the clients currently just for events easy to expand to
@@ -16,20 +13,18 @@ import javax.sound.sampled.FloatControl;
  * class to control audio
  */
 public class AudioController {
-
-  private Clip music;
-  private ArrayList<Clip> openClips;
-  private ClipCloser clipCloser;
   private int client;
+  private MediaPlayer mediaPlayer;
 
   public AudioController(int clientId) {
-    openClips = new ArrayList<>();
-    clipCloser = new ClipCloser(openClips);
-    clipCloser.start();
+    try {
+      mediaPlayer = new MediaPlayer(
+          new Media(getClass().getResource(Sounds.MENULOOP.getPath()).toURI().toString()));
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
     client = clientId;
   }
-
-
 
   /**
    * Sets the music volume
@@ -38,53 +33,7 @@ public class AudioController {
    */
   public void setMusicVolume(double musicVolume) {
     Settings.setMusicVolume(musicVolume);
-    refreshMusic();
-  }
-
-
-  /**
-   * This toggles the mute boolean on and off to be called by the com.lordsofmidnight.ui when a mute
-   * button is pressed (can become a Setter
-   */
-  public void toggleMute() {
-    Settings.setMute(!Settings.getMute());
-    refreshMusic();
-  }
-
-  /**
-   * Increases master game volume.
-   */
-  public void increaseVolume() {
-    if (Settings.getMusicVolume() < 1) {
-      Settings.setMusicVolume(Settings.getMusicVolume() + 0.1);
-    }
-
-    if (Settings.getSoundVolume() < 1) {
-      Settings.setSoundVolume(Settings.getSoundVolume() + 0.1);
-    }
-  }
-
-  /**
-   * Decreases master game volume.
-   */
-  public void decreaseVolume() {
-    if (Settings.getMusicVolume() >= 0.1) {
-      Settings.setMusicVolume(Settings.getMusicVolume() - 0.1);
-    }
-
-    if (Settings.getSoundVolume() >= 0.1) {
-      Settings.setSoundVolume(Settings.getSoundVolume() - 0.1);
-    }
-  }
-
-  /**
-   * Updates the volume of the music
-   */
-  private void refreshMusic() {
-    if (music == null) {
-      return;
-    }
-    setClipVolume(music, Settings.getMute() ? 0 : Settings.getMusicVolume());
+    mediaPlayer.setVolume(Settings.getMusicVolume());
   }
 
   /**
@@ -93,61 +42,24 @@ public class AudioController {
    * @param sound the sound to play
    */
   public void playSound(Sounds sound, int... id) {
-    if (Settings.getMute() || (id.length > 0 && id[0] != client)) {
+    if (Settings.getMute() /*|| (id.length > 0 && id[0] != client)*/) {
       return; // IF the com.lordsofmidnight.main has muted its audio nothing will be played
     }
-    long time = System.nanoTime();
     try {
       AudioClip newclip = new AudioClip(getClass().getResource(sound.getPath()).toURI().toString());
       newclip.setVolume(Settings.getSoundVolume());
       newclip.play();
-
-    /*InputStream audio = AudioController.class.getResourceAsStream(sound.getPath());
-    File audioFile = new File(sound.getPath());
-
-      AudioInputStream stream = AudioSystem.getAudioInputStream(audio);
-      DataLine.Info info = new DataLine.Info(Clip.class, stream.getFormat());
-      Clip clip = (Clip) AudioSystem.getLine(info);
-      clip.open(stream);
-      setClipVolume(clip, Settings.getSoundVolume());
-      clip.start();
-      openClips.add(clip); */
-      long oops = (System.nanoTime() - time);
-      if (oops > 4104100) {
-        System.out.println(oops + "       " + sound.getPath());
-      }
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   /**
-   * Sets the volume of the given clip
-   *
-   * @param clip The clip to set the volume of
-   * @param volume the volume (between 0.0 and 1.0)
-   */
-  public void setClipVolume(Clip clip, double volume) {
-    if (clip == null) {
-      // Change to logging once setup
-      System.err.println(" null clip sent to set volume");
-      return;
-    }
-    if (volume > 1.0) {
-      volume = 1.0;
-    }
-    if (volume < 0.1) {
-      volume = 0.0;
-    }
-    FloatControl control = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-    control.setValue((float) (Math.log(volume) / Math.log(10.0) * 20.0));
-  }
-
-  /**
    * Plays the game music intro the after the looping music
    */
   public void gameIntro() {
-    playMusic(Sounds.GAMEINTRO);
+    mediaPlayer.stop();
+    playSound(Sounds.GAMEINTRO);
     new Thread() {
       @Override
       public void run() {
@@ -167,64 +79,15 @@ public class AudioController {
    * @param sound file for the music
    */
   public void playMusic(Sounds sound) {
-    InputStream audio = AudioController.class.getResourceAsStream(sound.getPath());
-    //		File audioFile=new File(sound.getPath());
-
     try {
-      AudioInputStream stream = AudioSystem.getAudioInputStream(audio);
-      DataLine.Info info = new DataLine.Info(Clip.class, stream.getFormat());
-      Clip clip = (Clip) AudioSystem.getLine(info);
-      AudioSystem.getClip();
-      clip.open(stream);
-      if (music != null) {
-        music.stop();
-        music.close();
-      }
-      music = clip;
-      setClipVolume(music, Settings.getMusicVolume());
-      music.loop(Clip.LOOP_CONTINUOUSLY);
-      openClips.add(music);
+      mediaPlayer.stop();
+      mediaPlayer = new MediaPlayer(
+          new Media(getClass().getResource(sound.getPath()).toURI().toString()));
+      mediaPlayer.setVolume(Settings.getMusicVolume());
+      mediaPlayer.seek(Duration.ZERO);
+      mediaPlayer.play();
     } catch (Exception e) {
       e.printStackTrace();
-    }
-  }
-
-  /**
-   * A thread to close audio clips after they have been played
-   */
-  private class ClipCloser extends Thread {
-
-    private ArrayList<Clip> clips;
-    private Boolean running;
-
-    ClipCloser(ArrayList<Clip> clips) {
-      this.clips = clips;
-    }
-
-    @Override
-    public void run() {
-      running = true;
-      ArrayList<Clip> remove = new ArrayList<>();
-      while (running) { // closes any audio clips that have finished
-        remove.clear();
-        for (Clip clip : clips) {
-          if (!clip.isRunning()) {
-            remove.add(clip);
-          }
-        }
-        Clip x;
-        System.out.println("removing " + remove.size());
-        while (remove.size() > 0) {
-          x = remove.remove(0);
-          x.close();
-          clips.remove(x);
-        }
-        try {
-          Thread.sleep(5000);
-        } catch (Exception e) {
-          running = false;
-        }
-      }
     }
   }
 }
