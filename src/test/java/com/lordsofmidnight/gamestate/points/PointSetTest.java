2f09be02211b4ebd200b221ef54e25cd3ee23067
lja781 <lja781@cs.bham.ac.uk>
2019-03-28 11:03:15 +0000

    JUnit test classes

---
+++ src/test/java/com/lordsofmidnight/gamestate/points/PointSetTest.java
@@ -0,0 +1,68 @@
+package com.lordsofmidnight.gamestate.points;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointSetTest {
+
+  @Test
+  void getShallowClone() {
+  }
+
+  @Test
+  void contains() {
+  }
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void remove() {
+  }
+
+  @Test
+  void addAll() {
+  }
+
+  @Test
+  void removeAll() {
+  }
+
+  @Test
+  void retainAll() {
+  }
+
+  @Test
+  void containsAll() {
+  }
+
+  @Test
+  void toArray() {
+  }
+
+  @Test
+  void toArray1() {
+  }
+
+  @Test
+  void size() {
+  }
+
+  @Test
+  void clear() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+
+  @Test
+  void cloneTest() {
+  }
+
+  @Test
+  void iterator() {
+  }
+}
